4fca8e0521a64fd30f8fbb44ea79ce5729deef09
GerardoRufino <95055319+GerardoRufino@users.noreply.github.com>
2026-06-20 19:34:11 -0600

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3003',
+  baseURL: 'http://pro-gym-backend-8ejxmf-a73eed-3-19-0-140.sslip.io',
 });
 
 export default api;
